04bb117908426b7fe09f334982e130441765be59
Mateusz Grabowski <mateusz.grabowski@efficioconsulting.com>
2021-09-02 22:15:48 +0200

    Remove unused methods

--- src/Breaks.php
+++ src/Breaks.php
@@ -5,14 +5,4 @@ namespace App;
 interface Breaks
 {
     public function control(PressureSensor $pressureSensor, BreakPipe $breakPipe): void;
-
-//    public function loweringPressure(): void;
-//
-//    public function increasePressure(): void;
-//
-//    public function braking(): void;
-//
-//    public function release(): void;
-//
-//    public function emergencyBraking(): void;
 }
